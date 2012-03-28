fbd7f0d0dab5166b332919eae4b927e4a9827541
Andrew Crump <andrew@kotirisoftware.com>
2012-03-28 12:22:53 +0100

    FC015: Convert definitions to LWRP.

--- providers/instance.rb
+++ providers/instance.rb
@@ -16,8 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-actions :create do
-  include_recipe "memcached"
+action :create do
 
   runit_service "memcached-#{new_resource.name}" do
     template_name "memcached"
